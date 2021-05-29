05c91006d3b9a44481b85b4419df193799369dbf
Subrata Mazumder <subrata.besu@gmail.com>
2021-05-30 05:20:40 +0530

    try me button added

--- app/src/main/java/com/subrata/poc/android/helloworld/MainActivity.kt
+++ app/src/main/java/com/subrata/poc/android/helloworld/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.subrata.poc.android.helloworld
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val tryMeButton: Button = findViewById(R.id.button)
+        val imageVew: ImageView = findViewById(R.id.imageView)
+        val selfiImageRange = 1..4
+        tryMeButton.setOnClickListener{
+            val toast = Toast.makeText(this, "Trying your luck!!", Toast.LENGTH_SHORT)
+            toast.show()
+            val randomNumber = selfiImageRange.random()
+            if (randomNumber == 1)
+                imageVew.setImageResource(R.drawable.bdayselfi1)
+            else if (randomNumber == 2)
+                imageVew.setImageResource(R.drawable.bdayselfi2)
+            else if (randomNumber == 3)
+                imageVew.setImageResource(R.drawable.bdayselfi3)
+            else
+                imageVew.setImageResource(R.drawable.bdayselfi4)
+        }
     }
 }
